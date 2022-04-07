1451ce58a40eb3539b03705b8f76c3667a83d637
jinyaishida <100757861+jinyaishida@users.noreply.github.com>
2022-04-07 18:56:45 +0900

    first commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show, :destroy]
+
+
   def index
     @items = Item.all.order("created_at DESC")
   end
@@ -21,9 +23,21 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  #def edit
-    #@item = Item.find(params[:id])
-  #end
+  def edit
+    @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to action: :index
+  end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
 
   #def destroy
     #@item = Item.find(params[:id])
@@ -34,5 +48,7 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:title, :price, :text, :image, :description, :category_id, :shipping_date_id, :shipping_area_id, :shipping_method_id, :condition_id).merge(user_id: current_user.id)
   end
-  
+
+
 end
+  
