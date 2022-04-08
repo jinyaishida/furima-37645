1a7422b9fe7fcfacfb4c3da1b05084a7af34201c
jinyaishida <100757861+jinyaishida@users.noreply.github.com>
2022-04-08 11:30:30 +0900

    second commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show, :destroy]
 
 
   def index
@@ -38,8 +38,10 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy
-    redirect_to root_path
+      @item.destroy
+   unless current_user.id == @item.user_id
+      redirect_to root_path
+   end
   end
 
 
