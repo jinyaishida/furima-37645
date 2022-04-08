0bb6a8982874783a89f4a717ccad7771d301ccf8
jinyaishida <100757861+jinyaishida@users.noreply.github.com>
2022-04-08 11:45:00 +0900

    third commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,8 +38,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-      @item.destroy
    unless current_user.id == @item.user_id
+      @item.destroy
       redirect_to root_path
    end
   end
