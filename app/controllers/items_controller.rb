83571e68ecd61423f4f954724fefcb73befdb1d4
jinyaishida <100757861+jinyaishida@users.noreply.github.com>
2022-04-18 12:44:55 +0900

    second commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show, :destroy]
+  before_action :contributor_confirmation, only: [:edit, :update]
 
 
   def index
@@ -57,6 +58,10 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless @item.user == current_user
   end
 
 
